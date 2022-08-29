84f18dfdca9d401e7594cc6440d9735cb39a50f0
Tasyaaa <112340443+tsyaafarasyaa@users.noreply.github.com>
2022-08-29 22:38:38 +0700

    Update sourcode.js

--- plugins/sourcode.js
+++ plugins/sourcode.js
@@ -2,18 +2,17 @@
 // Jika ada yang Jual sc langsung Di hapus!!
 
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, 'https://telegra.ph/file/239f046a928370340401f.jpg',
+let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, 'https://telegra.ph/file/c58ccc276c39701a5ca2d.jpg',
 `Hi Kak @${m.sender.split('@')[0]} 
 
 ◪ 📮 *SCRIPT BOT*
 │ *Script :* 
-│ ╰ https://youtu.be/FHntJPk2PkU
-│ *Mampir Youtube Owner Banh:* 
-│ ╰ https://tinyurl.com/2qsejxnw
+│ ╰ Kepoan Jir🗿
+│ *Mampir Group Owner Banh:* 
+│ ╰ https://chat.whatsapp.com/IL34QSFDRT7LyAWiicXpBp
 ╰──────────═┅═──────────
 `,`📍 *N o t e :* 
-• Jangan lupa mampir Youtube Owner Kak!
-• Jangan Lupa kasih star & kasih credit
+• Jangan lupa mampir Group Owner Kak!
 
 Official By @${'0'.split('@')[0]}
 Powered By @${`${global.owner[0]}`.split('@')[0]}`, 'Pemilik Bot', '#owner')
